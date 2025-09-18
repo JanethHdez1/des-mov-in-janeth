96173d7b3e5549d64618c033ba28ab6467e86985
JanethHdez1 <20223tn061@utez.edu.mx>
2025-09-17 19:40:08 -0600

    Cambios clase worker y main

---
+++ workers_list/android/app/src/main/kotlin/com/example/workers_list/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.workers_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ workers_list/lib/Worker.dart
@@ -0,0 +1,13 @@
+class Worker {
+  final String id;
+  final String name;
+  final String lastname;
+  final int age;
+
+  Worker({
+    required this.id,
+    required this.name,
+    required this.lastname,
+    required this.age,
+  });
+}
